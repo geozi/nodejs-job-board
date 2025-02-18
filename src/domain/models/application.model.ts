0bcf08c1c9cba2cb2a51c60f2a8a97bfa6bb7d07
geozi <yorgosaz@gmail.com>
2025-02-18 11:41:01 +0200

    build: Added application.model.

---
+++ src/domain/models/application.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+import { IApplication } from "../interfaces/documents/iApplication.interface";
+import { commonConstants } from "../constants/common.constant";
+import { personFailedValidation } from "../messages/personValidation.message";
+import { listingFailedValidation } from "../messages/listingValidation.message";
+import { ID_REGEX } from "../resources/validationRegExp";
+
+const applicationSchema = new Schema<IApplication>(
+  {
+    personId: {
+      type: Schema.Types.ObjectId,
+      required: [true, personFailedValidation.PERSON_ID_REQUIRED_MESSAGE],
+      validate: {
+        validator: function (value: any) {
+          return value.length === commonConstants.MONGODB_ID_LENGTH;
+        },
+        message: personFailedValidation.PERSON_ID_OUT_OF_LENGTH_MESSAGE,
+      },
+      match: [ID_REGEX, personFailedValidation.PERSON_ID_INVALID_MESSAGE],
+    },
+    listingId: {
+      type: Schema.Types.ObjectId,
+      required: [true, listingFailedValidation.LISTING_ID_REQUIRED_MESSAGE],
+      validate: {
+        validator: function (value: any) {
+          return value.length === commonConstants.MONGODB_ID_LENGTH;
+        },
+        message: listingFailedValidation.LISTING_ID_OUT_OF_LENGTH_MESSAGE,
+      },
+      match: [ID_REGEX, listingFailedValidation.LISTING_ID_INVALID_MESSAGE],
+    },
+  },
+  {
+    collection: "applications",
+    timestamps: true,
+  }
+);
+
+export const applicationModel = model<IApplication>(
+  "Application",
+  applicationSchema
+);
