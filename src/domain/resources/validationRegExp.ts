93e858b218e5ec417f7593d7e85bec6448635774
geozi <yorgosaz@gmail.com>
2025-02-18 18:10:11 +0200

    build: Added DATE_REGEX to validationRegExp.

--- src/domain/resources/validationRegExp.ts
+++ src/domain/resources/validationRegExp.ts
@@ -13,3 +13,5 @@ export const COUNTRY_REGEX = new RegExp(/^[A-Za-z ]+$/);
 export const PHONE_REGEX = new RegExp(/^\d+(-\d+)*$/);
 
 export const ID_REGEX = new RegExp(/^[a-f0-9]+$/);
+
+export const DATE_REGEX = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
