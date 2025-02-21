70fb8e630800c58d596802963734ff3fcbd1f775
geozi <yorgosaz@gmail.com>
2025-02-21 12:16:44 +0200

    build: Added updateUser and deleteUser functionality to user.repository.

--- src/persistence/user.repository.ts
+++ src/persistence/user.repository.ts
@@ -3,29 +3,60 @@ import { IUser } from "../domain/interfaces/documents/iUser.interface";
 import { RoleType } from "../domain/enums/roleType.enum";
 import { Types } from "mongoose";
 import { appLogger } from "../../logs/logger.config";
+import { IUserUpdate } from "../business/interfaces/IUserUpdate.interface";
 
 export const getUserByUsername = async (
   username: string
 ): Promise<IUser | null> => {
   const requestedUser = await User.findOne({ username: username });
+
   appLogger.info(`User repository: ${User.find.name} called successfully`);
+
   return requestedUser;
 };
 
 export const getUserByEmail = async (email: string): Promise<IUser | null> => {
   const requestedUser = await User.findOne({ email: email });
+
   appLogger.info(`User repository: ${User.find.name} called successfully`);
+
   return requestedUser;
 };
 
 export const getUsersByRole = async (role: RoleType): Promise<Array<IUser>> => {
   const requestedUsers = await User.find({ role: role });
+
   appLogger.info(`User repository: ${User.find.name} called successfully`);
+
   return requestedUsers;
 };
 
 export const addUser = async (newUser: IUser): Promise<IUser> => {
   const savedUser = await newUser.save();
+
   appLogger.info(`User repository: ${newUser.save.name} called successfully`);
+
   return savedUser;
+};
+
+export const updateUser = async (
+  updateDataObj: IUserUpdate
+): Promise<IUser | null> => {
+  const updatedUser = await User.findByIdAndUpdate(
+    updateDataObj.id,
+    updateDataObj,
+    { new: true, runValidators: true, context: "query" }
+  );
+
+  appLogger.info(`User repository: findByIdAndUpdate called successfully`);
+
+  return updatedUser;
+};
+
+export const deleteUser = async (id: Types.ObjectId): Promise<IUser | null> => {
+  const deletedUser = await User.findByIdAndDelete(id);
+
+  appLogger.info(`User repository: findByIdAndDelete called successfully`);
+
+  return deletedUser;
 };
