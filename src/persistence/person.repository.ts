720175b7b56d266d8de698c8992feb7d7c3c3bc3
geozi <yorgosaz@gmail.com>
2025-02-21 17:53:47 +0200

    build: Added person.repository.

---
+++ src/persistence/person.repository.ts
@@ -0,0 +1,62 @@
+import { Person } from "../domain/models/person.model";
+import { IPerson } from "../domain/interfaces/documents/iPerson.interface";
+import { Types } from "mongoose";
+import { appLogger } from "../../logs/logger.config";
+import { IPersonUpdate } from "../business/interfaces/iPersonUpdate.interface";
+
+export const getPersonByUsername = async (username: string) => {
+  const requestedPerson = await Person.find({ username: username });
+
+  appLogger.info(`Person repository: ${Person.find.name} called successfully`);
+
+  return requestedPerson;
+};
+
+export const addPerson = async (newPerson: IPerson) => {
+  const savedPerson = await newPerson.save();
+
+  appLogger.info(
+    `Person repository: ${savedPerson.save.name} called successfully`
+  );
+
+  return savedPerson;
+};
+
+export const updatePerson = async (updateDataObj: IPersonUpdate) => {
+  const {
+    id,
+    firstName,
+    lastName,
+    phoneNumber,
+    address,
+    dateOfBirth,
+    education,
+    workExperience,
+    username,
+  } = { ...updateDataObj };
+
+  const personToUpdate = {
+    firstName: firstName,
+    lastName: lastName,
+    phoneNumber: phoneNumber,
+    address: address,
+    dateOfBirth: dateOfBirth,
+    education: education,
+    workExperience: workExperience,
+    username: username,
+  };
+
+  const updatedPerson = await Person.findByIdAndUpdate(id, personToUpdate);
+
+  appLogger.info(`Person repository: findByIdAndUpdate called successfully`);
+
+  return updatedPerson;
+};
+
+export const deletePerson = async (id: Types.ObjectId) => {
+  const deletedPerson = await Person.findByIdAndDelete(id);
+
+  appLogger.info(`Person repository: findByIdAndDelete called successfully`);
+
+  return deletedPerson;
+};
