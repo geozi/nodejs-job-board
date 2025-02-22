939e57e9b4d2b9f05751848a8469808ab36e7492
geozi <yorgosaz@gmail.com>
2025-02-22 17:27:45 +0200

    build: Added uniqueness constraint to application.model for personId.

--- src/domain/models/application.model.ts
+++ src/domain/models/application.model.ts
@@ -8,6 +8,7 @@ import { commonConstants } from "../constants/common.constant";
 import { personFailedValidation } from "../messages/personValidation.message";
 import { listingFailedValidation } from "../messages/listingValidation.message";
 import { ID_REGEX } from "../resources/validationRegExp";
+import mongooseUniqueValidator from "mongoose-unique-validator";
 
 /**
  * Application schema for persistence in MongoDB.
@@ -20,6 +21,7 @@ const applicationSchema = new Schema<IApplication>(
   {
     personId: {
       type: Schema.Types.ObjectId,
+      unique: true,
       required: [true, personFailedValidation.PERSON_ID_REQUIRED_MESSAGE],
       validate: {
         validator: function (value: any) {
@@ -46,6 +48,11 @@ const applicationSchema = new Schema<IApplication>(
     timestamps: true,
   }
 );
+
+applicationSchema.plugin(mongooseUniqueValidator, {
+  message: "{PATH} already exists in the database",
+  type: "UniqueConstraintError",
+});
 
 export const Application = model<IApplication>(
   "Application",
