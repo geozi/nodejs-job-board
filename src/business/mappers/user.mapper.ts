9ef720d642f32df9205da0a5d011225153337b79
geozi <yorgosaz@gmail.com>
2025-02-26 13:22:21 +0200

    build: Added user.mapper.

---
+++ src/business/mappers/user.mapper.ts
@@ -0,0 +1,54 @@
+import { Request } from "express";
+import bcrypt from "bcryptjs";
+import { RoleType } from "../../domain/enums/roleType.enum";
+import { User } from "../../domain/models/user.model";
+import { IUserUpdate } from "../interfaces/IUserUpdate.interface";
+import { IUser } from "../../domain/interfaces/documents/iUser.interface";
+
+export const reqBodyToUser = async (req: Request): Promise<IUser> => {
+  const { username, email, password, role } = req.body;
+  const user = new User({
+    username: username,
+    email: email,
+  });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  user.password = hashedPassword;
+
+  switch (role) {
+    case RoleType.Admin.toString():
+      user.role = RoleType.Admin;
+      break;
+    case RoleType.User.toString():
+      user.role = RoleType.User;
+      break;
+  }
+
+  return user;
+};
+
+export const reqBodyToUserUpdate = async (
+  req: Request
+): Promise<IUserUpdate> => {
+  const { id, username, email, password, role } = req.body;
+
+  const userToUpdate: IUserUpdate = {
+    id: id,
+    username: username,
+    email: email,
+  };
+
+  const hashedPassword = password ? await bcrypt.hash(password, 10) : undefined;
+  userToUpdate.password = hashedPassword;
+
+  switch (role) {
+    case RoleType.Admin.toString():
+      userToUpdate.role = RoleType.Admin;
+      break;
+    case RoleType.User.toString():
+      userToUpdate.role = RoleType.User;
+      break;
+  }
+
+  return userToUpdate;
+};
