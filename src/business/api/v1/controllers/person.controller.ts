2e7a45bbdd8d1b7e562f600abdfd24ed800b0fe3
geozi <yorgosaz@gmail.com>
2025-03-01 11:51:29 +0200

    build: Added personInfoUpdateRules to person.rules and infoUpdateMiddlewareArray to person.controller.

--- src/business/api/v1/controllers/person.controller.ts
+++ src/business/api/v1/controllers/person.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
-import { personInfoCreationRules } from "../middleware/person.rules";
+import {
+  personInfoCreationRules,
+  personInfoUpdateRules,
+} from "../middleware/person.rules";
 import { validationResult } from "express-validator";
 import { httpCodes } from "../../../codes/responseStatusCodes";
 import { appLogger } from "../../../../../logs/logger.config";
 import { commonResponseMessages } from "../../../messages/commonResponse.message";
 import { ServerError } from "../../../../errors/serverError.class";
 import { UniqueConstraintError } from "../../../../errors/uniqueConstraintError.class";
-import { reqBodyToPerson } from "../../../mappers/person.mapper";
+import {
+  reqBodyToPerson,
+  reqBodyToPersonUpdate,
+} from "../../../mappers/person.mapper";
 import { personControllerResponseMessages } from "../../../messages/personControllerResponse.message";
-import { createPersonInfo } from "../../../../service/person.service";
+import {
+  bringPersonInfoToDate,
+  createPersonInfo,
+} from "../../../../service/person.service";
+import { NotFoundError } from "../../../../errors/notFoundError.class";
 
 export const infoCreationMiddlewareArray = [
   ...personInfoCreationRules(),
@@ -44,6 +54,49 @@ export const infoCreationMiddlewareArray = [
       ) {
         appLogger.error(
           `User controller: ${callPersonInfoCreation.name} -> ${error.name} detected and caught`
+        );
+
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const infoUpdateMiddlewareArray = [
+  ...personInfoUpdateRules(),
+  async function callPersonInfoUpdate(req: Request, res: Response) {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      appLogger.error(
+        `User controller: ${callPersonInfoUpdate.name} -> Express validation errors detected and caught`
+      );
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: commonResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const userToUpdate = reqBodyToPersonUpdate(req);
+      const updatedUser = await bringPersonInfoToDate(userToUpdate);
+
+      res
+        .status(httpCodes.OK)
+        .json({
+          message: personControllerResponseMessages.PERSON_UPDATED,
+          data: updatedUser,
+        });
+    } catch (error) {
+      if (error instanceof ServerError || error instanceof NotFoundError) {
+        appLogger.error(
+          `User controller: ${callPersonInfoUpdate.name} -> ${error.name} detected and caught`
         );
 
         res.status(error.httpCode).json({ message: error.message });
--- src/business/api/v1/middleware/person.rules.ts
+++ src/business/api/v1/middleware/person.rules.ts
@@ -3,6 +3,7 @@ import { personFailedValidation } from "../../../../domain/messages/personValida
 import { personConstants } from "../../../../domain/constants/person.constant";
 import {
   DATE_REGEX,
+  ID_REGEX,
   NAME_REGEX,
   PHONE_REGEX,
 } from "../../../../domain/resources/validationRegExp";
@@ -49,6 +50,53 @@ export const personInfoCreationRules = (): ValidationChain[] => {
     check("username")
       .notEmpty()
       .withMessage(userFailedValidation.USERNAME_REQUIRED_MESSAGE)
+      .isLength({ min: userConstants.USERNAME_MIN_LENGTH })
+      .withMessage(userFailedValidation.USERNAME_BELOW_MIN_LENGTH_MESSAGE)
+      .isLength({ max: userConstants.USERNAME_MAX_LENGTH })
+      .withMessage(userFailedValidation.USERNAME_ABOVE_MAX_LENGTH_MESSAGE),
+  ];
+};
+
+export const personInfoUpdateRules = (): ValidationChain[] => {
+  return [
+    check("id")
+      .notEmpty()
+      .withMessage(personFailedValidation.PERSON_ID_REQUIRED_MESSAGE)
+      .isLength({
+        min: commonConstants.MONGODB_ID_LENGTH,
+        max: commonConstants.MONGODB_ID_LENGTH,
+      })
+      .withMessage(personFailedValidation.PERSON_ID_OUT_OF_LENGTH_MESSAGE)
+      .matches(ID_REGEX)
+      .withMessage(personFailedValidation.PERSON_ID_INVALID_MESSAGE),
+    check("firstName")
+      .optional()
+      .isLength({ min: personConstants.PERSON_NAME_MIN_LENGTH })
+      .withMessage(personFailedValidation.FIRST_NAME_BELOW_MIN_LENGTH_MESSAGE)
+      .matches(NAME_REGEX)
+      .withMessage(personFailedValidation.FIRST_NAME_INVALID_MESSAGE),
+    check("lastName")
+      .optional()
+      .isLength({ min: personConstants.PERSON_NAME_MIN_LENGTH })
+      .withMessage(personFailedValidation.LAST_NAME_BELOW_MIN_LENGTH_MESSAGE)
+      .matches(NAME_REGEX)
+      .withMessage(personFailedValidation.LAST_NAME_INVALID_MESSAGE),
+    check("phoneNumber")
+      .optional()
+      .matches(PHONE_REGEX)
+      .withMessage(personFailedValidation.PHONE_NUMBER_INVALID_MESSAGE),
+    check("address")
+      .optional()
+      .isLength({ min: commonConstants.GENERIC_MIN_LENGTH })
+      .withMessage(personFailedValidation.ADDRESS_BELOW_MIN_LENGTH_MESSAGE),
+    check("dateOfBirth")
+      .optional()
+      .matches(DATE_REGEX)
+      .withMessage(personFailedValidation.DATE_OF_BIRTH_INVALID_MESSAGE),
+    check("education").optional(),
+    check("workExperience").optional(),
+    check("username")
+      .optional()
       .isLength({ min: userConstants.USERNAME_MIN_LENGTH })
       .withMessage(userFailedValidation.USERNAME_BELOW_MIN_LENGTH_MESSAGE)
       .isLength({ max: userConstants.USERNAME_MAX_LENGTH })
