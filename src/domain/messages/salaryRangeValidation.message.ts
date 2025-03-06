a5f46f25d2620182494c6a5e82559435bfe61bd2
geozi <yorgosaz@gmail.com>
2025-03-06 11:19:18 +0200

    refactor: Removed MIN_AMOUNT_REQUIRED and MAX_AMOUNT_REQUIRED messages from salaryRangeValidation.message.

--- src/domain/messages/salaryRangeValidation.message.ts
+++ src/domain/messages/salaryRangeValidation.message.ts
@@ -8,20 +8,12 @@ import { ISalaryRange } from "domain/interfaces/secondary/iSalaryRange.interface
  * Contains error message(s) that are used when validation of {@link ISalaryRange}-complied objects fails.
  *
  * @type {object}
- * @property {string} MIN_AMOUNT_REQUIRED_MESSAGE - Message sent when no min amount is provided.
  * @property {string} MIN_AMOUNT_NEGATIVE_MESSAGE - Message sent when the provided min amount is negative.
  * @property {string} MIN_AMOUNT_INVALID_MESSAGE - Message sent when the provided min amount is not a number.
- * @property {string} MAX_AMOUNT_REQUIRED_MESSAGE - Message sent when no max amount is provided.
  * @property {string} MAX_AMOUNT_NEGATIVE_MESSAGE - Message sent when the provided max amount is negative.
  * @property {string} MAX_AMOUNT_INVALID_MESSAGE - Message sent when the provided max amount is not a number.
  */
 export const salaryRangeFailedValidation = {
-  /**
-   * Message sent when no min amount is provided.
-   * @type {string}
-   */
-  MIN_AMOUNT_REQUIRED_MESSAGE: "Min amount is a required field",
-
   /**
    * Message sent when the provided min amount is negative.
    * @type {string}
@@ -33,12 +25,6 @@ export const salaryRangeFailedValidation = {
    * @type {string}
    */
   MIN_AMOUNT_INVALID_MESSAGE: "Min amount must be a number",
-
-  /**
-   * Message sent when no max amount is provided.
-   * @type {string}
-   */
-  MAX_AMOUNT_REQUIRED_MESSAGE: "Max amount is a required field",
 
   /**
    * Message sent when the provided max amount is negative.
