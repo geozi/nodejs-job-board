bbc4e03ed522d8b14779e90d9602b01aa2b21eaa
geozi <yorgosaz@gmail.com>
2025-03-07 09:56:00 +0200

    build: Added nodemon and dotenv dependencies; added database connection to app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,21 @@
-let someTest = "something";
-console.log(someTest);
+import * as dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import express from "express";
+const app = express();
+const port = 3000;
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("Connection to database established.");
+  } catch (error) {
+    console.log("Failed to connect to database", error);
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
+
+connectToDB();
