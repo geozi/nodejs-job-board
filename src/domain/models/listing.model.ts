5639c4428a01af4f8cf4d9ab373898870e7d620a
geozi <yorgosaz@gmail.com>
2025-03-07 09:36:29 +0200

    fix: Added collection reference to listing.model.

--- src/domain/models/listing.model.ts
+++ src/domain/models/listing.model.ts
@@ -30,87 +30,102 @@ import { listingStatusArray } from "domain/enums/listingStatus.enum";
  * @property {Schema.Types.Mixed[]} [salaryRange] - (Optional) The salary range of the job.
  * @property {string} status - The status of the listing.
  */
-const listingSchema = new Schema<IListing>({
-  title: {
-    type: String,
-    required: [true, listingFailedValidation.TITLE_REQUIRED_MESSAGE],
-    trim: true,
-  },
-  organizationName: {
-    type: String,
-    required: [true, commonFailedValidation.ORGANIZATION_NAME_REQUIRED_MESSAGE],
-    minLength: [
-      commonConstants.ORGANIZATION_NAME_MIN_LENGTH,
-      commonFailedValidation.ORGANIZATION_NAME_MIN_LENGTH_MESSAGE,
-    ],
-    trim: true,
-  },
-  datePosted: {
-    type: Schema.Types.Date,
-    required: [true, listingFailedValidation.DATE_POSTED_REQUIRED_MESSAGE],
-  },
-  workType: {
-    type: String,
-    required: [true, listingFailedValidation.WORK_TYPE_REQUIRED_MESSAGE],
-    enum: {
-      values: workTypeArray,
-      message: listingFailedValidation.WORK_TYPE_INVALID_MESSAGE,
+const listingSchema = new Schema<IListing>(
+  {
+    title: {
+      type: String,
+      required: [true, listingFailedValidation.TITLE_REQUIRED_MESSAGE],
+      trim: true,
+    },
+    organizationName: {
+      type: String,
+      required: [
+        true,
+        commonFailedValidation.ORGANIZATION_NAME_REQUIRED_MESSAGE,
+      ],
+      minLength: [
+        commonConstants.ORGANIZATION_NAME_MIN_LENGTH,
+        commonFailedValidation.ORGANIZATION_NAME_MIN_LENGTH_MESSAGE,
+      ],
+      trim: true,
+    },
+    datePosted: {
+      type: Schema.Types.Date,
+      required: [true, listingFailedValidation.DATE_POSTED_REQUIRED_MESSAGE],
+    },
+    workType: {
+      type: String,
+      required: [true, listingFailedValidation.WORK_TYPE_REQUIRED_MESSAGE],
+      enum: {
+        values: workTypeArray,
+        message: listingFailedValidation.WORK_TYPE_INVALID_MESSAGE,
+      },
+    },
+    employmentType: {
+      type: String,
+      required: [
+        true,
+        listingFailedValidation.EMPLOYMENT_TYPE_REQUIRED_MESSAGE,
+      ],
+      enum: {
+        values: employmentTypeArray,
+        message: listingFailedValidation.EMPLOYMENT_TYPE_INVALID_MESSAGE,
+      },
+    },
+    experienceLevel: {
+      type: String,
+      required: [
+        true,
+        listingFailedValidation.EXPERIENCE_LEVEL_REQUIRED_MESSAGE,
+      ],
+      enum: {
+        values: experienceLevelTypeArray,
+        message: listingFailedValidation.EXPERIENCE_LEVEL_INVALID_MESSAGE,
+      },
+    },
+    city: {
+      type: String,
+      required: [true, listingFailedValidation.CITY_REQUIRED_MESSAGE],
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: [true, listingFailedValidation.COUNTRY_REQUIRED_MESSAGE],
+      match: [COUNTRY_REGEX, commonFailedValidation.COUNTRY_INVALID_MESSAGE],
+      trim: true,
+    },
+    listingDesc: {
+      type: String,
+      required: [
+        true,
+        listingFailedValidation.LISTING_DESCRIPTION_REQUIRED_MESSAGE,
+      ],
+      minLength: [
+        commonConstants.GENERIC_MIN_LENGTH,
+        listingFailedValidation.LISTING_DESCRIPTION_BELOW_MIN_LENGTH_MESSAGE,
+      ],
+      maxLength: [
+        listingConstants.LISTING_DESCRIPTION_MAX_LENGTH,
+        listingFailedValidation.LISTING_DESCRIPTION_ABOVE_MAX_LENGTH_MESSAGE,
+      ],
+      trim: true,
+    },
+    salaryRange: {
+      type: Schema.Types.Mixed,
+    },
+    status: {
+      type: String,
+      required: [true, listingFailedValidation.STATUS_REQUIRED],
+      enum: {
+        values: listingStatusArray,
+        message: listingFailedValidation.STATUS_INVALID,
+      },
     },
   },
-  employmentType: {
-    type: String,
-    required: [true, listingFailedValidation.EMPLOYMENT_TYPE_REQUIRED_MESSAGE],
-    enum: {
-      values: employmentTypeArray,
-      message: listingFailedValidation.EMPLOYMENT_TYPE_INVALID_MESSAGE,
-    },
-  },
-  experienceLevel: {
-    type: String,
-    required: [true, listingFailedValidation.EXPERIENCE_LEVEL_REQUIRED_MESSAGE],
-    enum: {
-      values: experienceLevelTypeArray,
-      message: listingFailedValidation.EXPERIENCE_LEVEL_INVALID_MESSAGE,
-    },
-  },
-  city: {
-    type: String,
-    required: [true, listingFailedValidation.CITY_REQUIRED_MESSAGE],
-    trim: true,
-  },
-  country: {
-    type: String,
-    required: [true, listingFailedValidation.COUNTRY_REQUIRED_MESSAGE],
-    match: [COUNTRY_REGEX, commonFailedValidation.COUNTRY_INVALID_MESSAGE],
-    trim: true,
-  },
-  listingDesc: {
-    type: String,
-    required: [
-      true,
-      listingFailedValidation.LISTING_DESCRIPTION_REQUIRED_MESSAGE,
-    ],
-    minLength: [
-      commonConstants.GENERIC_MIN_LENGTH,
-      listingFailedValidation.LISTING_DESCRIPTION_BELOW_MIN_LENGTH_MESSAGE,
-    ],
-    maxLength: [
-      listingConstants.LISTING_DESCRIPTION_MAX_LENGTH,
-      listingFailedValidation.LISTING_DESCRIPTION_ABOVE_MAX_LENGTH_MESSAGE,
-    ],
-    trim: true,
-  },
-  salaryRange: {
-    type: Schema.Types.Mixed,
-  },
-  status: {
-    type: String,
-    required: [true, listingFailedValidation.STATUS_REQUIRED],
-    enum: {
-      values: listingStatusArray,
-      message: listingFailedValidation.STATUS_INVALID,
-    },
-  },
-});
+  {
+    collection: "listings",
+    timestamps: true,
+  }
+);
 
 export const Listing = model<IListing>("Listing", listingSchema);
